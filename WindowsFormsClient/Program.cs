ba413e588533cd57093801541781cdc9b0fa9404
ViktorovLeonid <70852092+ViktorovLeonid@users.noreply.github.com>
2022-01-10 00:00:22 +0300

    Update Program.cs

--- WindowsFormsClient/Program.cs
+++ WindowsFormsClient/Program.cs
@@ -1,22 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsClient
+namespace WindowsFormsApp1
 {
-    static class Program
+  static class Program
+  {
+    /// <summary>
+    /// Главная точка входа для приложения.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
     }
+  }
 }
