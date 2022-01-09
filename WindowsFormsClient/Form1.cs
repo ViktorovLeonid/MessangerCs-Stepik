e77151701f2b66963aebd052d50e7ff176d336a7
ViktorovLeonid <70852092+ViktorovLeonid@users.noreply.github.com>
2022-01-09 23:59:21 +0300

    Update Form1.cs

--- WindowsFormsClient/Form1.cs
+++ WindowsFormsClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using ConsoleMessenger;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ConsoleMessenger;
 
-namespace WindowsFormsClient
+namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    private static int MessageID;
+    private static string UserName;
+    private static MessangerClientAPI API = new MessangerClientAPI();
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string UserName = UserNameTB.Text;
+      string Message = MessageTB.Text;
+      if ((UserName.Length > 1) && (UserName.Length > 1))
+      {
+        ConsoleMessenger.Message msg = new ConsoleMessenger.Message(UserName, Message, DateTime.Now);
+        API.SendMessageRestSharp(msg);
+      }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+
+      var getMessage = new Func<Task>(async () =>
+      {
+        ConsoleMessenger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+        while (msg != null)
+        {
+          MessagesLB.Items.Add(msg);
+          MessageID++;
+          msg = await API.GetMessageHTTPAsync(MessageID);
+        }
+      });
+      getMessage.Invoke();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      MessageID = 0;
+    }
+
+    private void MessagesLB_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
 }
